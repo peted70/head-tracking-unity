684636e8f4a2e5002c2a493ae76498306ca7cadf
peted <peted70@hotmail.com>
2015-01-08 19:30:54 +0000

    added unity project

---
+++ Assets/HeadTrack.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadTrack : MonoBehaviour
+{
+    private bool gyroBool;
+    private Gyroscope gyro;
+    private Quaternion rotFix;
+    private Vector3 initial = new Vector3(90, 180, 0);
+        
+    // Use this for initialization
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        gyroBool = SystemInfo.supportsGyroscope;
+
+        Debug.Log("gyro bool = " + gyroBool.ToString());
+
+        if (gyroBool)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            rotFix = new Quaternion(0, 0, 0.7071f, 0.7071f);
+        }
+        else
+        {
+            Debug.Log("No Gyro Support");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gyroBool)
+        {
+            var camRot = gyro.attitude * rotFix;
+            transform.eulerAngles = initial;
+            transform.localRotation *= camRot;
+        }
+    }
+}
---
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/MobileInput.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileInput : MonoBehaviour
+{
+    public static int HorizontalAxis { get; set; }
+    public static int VerticalAxis { get; set; }
+}
